761bb928bb39e72cf5578ff7d6b4307e21b4b472
Ananthak <Ananthakrishna.K@iiitb.ac.in>
2023-08-30 09:52:07 +0530

    action chains

--- main.py
+++ main.py
@@ -2,24 +2,36 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 
 options =  webdriver.ChromeOptions()
 options.add_experimental_option("detach",True)
-
 driver = webdriver.Chrome(options=options)
 
 wait = WebDriverWait(driver,timeout=5)
 
-driver.get("https://www.wikipedia.org")
+driver.get("https://orteil.dashnet.org/cookieclicker/")
 print(driver.title)
 
-search = driver.find_element(By.ID,'searchInput')
-wait.until(lambda d:search.is_displayed())
-search.send_keys("modi")
-search.send_keys(Keys.RETURN)
-list = driver.find_element(By.ID,"mw-panel-toc-list")
-wait.until(lambda d:list.is_displayed())
-print(list.text)
-""" driver.quit() """
+driver.implicitly_wait(10)
+driver.find_element(By.ID,"langSelect-EN").click()
+driver.implicitly_wait(10)
+cookie = driver.find_element(By.ID,"bigCookie")
+cookie_count = driver.find_element(By.ID,"cookies")
+items = [driver.find_element(By.ID,"product"+str(i)) for i in range(1,-1,-1)] 
+
+
+for i in range(5000):
+    actions = ActionChains(driver)
+    actions.click(cookie)
+    actions.perform()
+    count = int(cookie_count.text.split()[0])
+    for item in items:
+        value = int(item.text.split()[1])
+        if value <=count:
+            upgrade = ActionChains(driver)
+            upgrade.move_to_element(item)
+            upgrade.click()
+            upgrade.perform()
 
 
