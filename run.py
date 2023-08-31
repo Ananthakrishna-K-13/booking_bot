f1250cbd96af2a563f72214e33b6191f7e99cb6e
Ananthak <Ananthakrishna.K@iiitb.ac.in>
2023-08-31 22:02:03 +0530

    bot automation started

---
+++ run.py
@@ -0,0 +1,9 @@
+from booking.booking import Booking
+
+bot = Booking()
+bot.land_first_page()
+bot.changeCurrency()
+bot.select_place_to_go("New York")
+bot.select_dates(checkin="2023-09-04",checkout="2023-09-09")
+bot.select_occupancy(7)
+bot.click_search()
